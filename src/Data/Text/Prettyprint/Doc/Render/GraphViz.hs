06d793964414f6c41c42e35251eaf8c76c12f0e2
George Thomas <georgefsthomas@gmail.com>
2020-06-11 13:06:12 +0100

    SImplify by using generic stack machine helper

--- src/Data/Text/Prettyprint/Doc/Render/GraphViz.hs
+++ src/Data/Text/Prettyprint/Doc/Render/GraphViz.hs
@@ -1,94 +1,66 @@
 -- | Pretty-print GraphViz labels
 module Data.Text.Prettyprint.Doc.Render.GraphViz (
-    -- * Rendering functions
     render,
     render',
-
-    -- * Error handling
-    -- | 'render' and 'render'' can throw errors, given a malformed document stream.
-    -- The average user is very unlikely to run into this,
-    -- but error handling functionality is provided for completeness.
-    GraphVizRenderError(..),
-    renderSafe,
-    renderSafe',
 ) where
 
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text as T
 
-import Control.Exception (
-    Exception,
-    displayException,
-    throw,
-    )
-import Data.GraphViz.Attributes.Complete (
-    Label (HtmlLabel),
-    )
+import Data.GraphViz.Attributes.Complete (Label(HtmlLabel))
 import qualified Data.GraphViz.Attributes.HTML as H
+import Data.Text (Text)
 import Data.Text.Prettyprint.Doc (
-        Doc,
-        SimpleDocStream (..),
-        defaultLayoutOptions,
-        layoutPretty,
+    Doc,
+    SimpleDocStream(
+        SFail,
+        SEmpty,
+        SChar,
+        SText,
+        SLine,
+        SAnnPush,
+        SAnnPop),
+    layoutPretty, defaultLayoutOptions,
+    )
+import Data.Text.Prettyprint.Doc.Internal (textSpaces)
+import Data.Text.Prettyprint.Doc.Render.Util.Panic (
+    panicUnpairedPop, panicInputNotFullyConsumed, panicUncaughtFail
     )
 
--- | Render a document as a GraphViz label, using 'defaultLayoutOptions'.
+-- | Render a document as a GraphViz label, using sensible defaults.
 render :: Doc H.Attribute -> Label
-render = throwLeft . renderSafe
+render = HtmlLabel . H.Text . render' . layoutPretty defaultLayoutOptions
 
 -- | Render a document stream as HTML text for GraphViz. This provides more fine-grained control than 'render'.
 render' :: SimpleDocStream H.Attribute -> H.Text
-render' = throwLeft . renderSafe'
-
--- | Errors which indicate a malformed 'SimpleDocStream'.
-data GraphVizRenderError
-    -- | The stream ended with 'SFail'.
-    = GVDocStreamFail
-    -- | The stream contained more 'SAnnPop's than 'SAnnPush's.
-    | GVEmptyStack
-    deriving (Eq, Ord, Read, Show)
-instance Exception GraphVizRenderError where
-    displayException = \case
-        GVDocStreamFail -> t ++ "encountered failure in document stream"
-        GVEmptyStack    -> t ++ "attempted to pop empty attribute stack"
-        where t = "Failed to render HTML for GraphViz: "
-
--- | A total version of 'render'.
-renderSafe :: Doc H.Attribute -> Either GraphVizRenderError Label
-renderSafe = fmap (HtmlLabel . H.Text) . renderSafe' . layoutPretty defaultLayoutOptions
-
--- | A total version of 'render''.
--- This can be seen as a generalisation of any of the other functions exported by this module.
-renderSafe' :: SimpleDocStream H.Attribute -> Either GraphVizRenderError H.Text
-renderSafe' =
-    let go cs = \case
-            SFail           -> Left GVDocStreamFail
-            SEmpty          -> Right []
-            SChar c ds      -> renderText cs (T.singleton c) ?: go cs ds
-            SText _ txt ds  -> renderText cs txt ?: go cs ds
-            SLine n ds      -> H.Newline [] ?: renderText cs (T.replicate n " ") ?: go cs ds
-            SAnnPush col ds -> go (col : cs) ds
-            SAnnPop ds      -> flip go ds =<< maybeToEither GVEmptyStack (tailMay cs)
-        infixr 0 ?:
-        (?:) = fmap . (:)
-    in  go []
+render' = renderSimplyDecorated' (pure .: renderText) mempty mempty
 
 
-{- Internal utilities -}
+{- Util -}
 
--- | On encountering a 'Left', throw it as an exception.
-throwLeft :: Exception e => Either e a -> a
-throwLeft = either throw id
+(.:) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
+(.:) = (.) . (.)
 
--- | Equal to the function of the same name from [extra](https://hackage.haskell.org/package/extra).
-maybeToEither :: a -> Maybe b -> Either a b
-maybeToEither x = maybe (Left x) Right
-
--- | Equal to the function of the same name from [safe](https://hackage.haskell.org/package/safe).
-tailMay :: [a] -> Maybe [a]
-tailMay = \case
-    []     -> Nothing
-    _ : xs -> Just xs
+-- | This is a very minor modification of 'renderSimplyDecorated', where the /text/ function is
+-- additionally passed the current stack. Worth suggesting change upstream.
+renderSimplyDecorated'
+    :: Monoid out
+    => ([ann] -> Text -> out) -- ^ Render plain 'Text'
+    -> (ann -> out)  -- ^ How to render an annotation
+    -> (ann -> out)  -- ^ How to render the removed annotation
+    -> SimpleDocStream ann
+    -> out
+renderSimplyDecorated' text push pop = go []
+  where
+    go _           SFail               = panicUncaughtFail
+    go []          SEmpty              = mempty
+    go (_:_)       SEmpty              = panicInputNotFullyConsumed
+    go stack       (SChar c rest)      = text stack (T.singleton c) <> go stack rest
+    go stack       (SText _l t rest)   = text stack t <> go stack rest
+    go stack       (SLine i rest)      = text stack (T.singleton '\n') <> text stack (textSpaces i) <> go stack rest
+    go stack       (SAnnPush ann rest) = push ann <> go (ann : stack) rest
+    go (ann:stack) (SAnnPop rest)      = pop ann <> go stack rest
+    go []          SAnnPop{}           = panicUnpairedPop
 
 -- | Helper for rendering an individual 'H.TextItem'.
 renderText :: H.Attributes -> T.Text -> H.TextItem
