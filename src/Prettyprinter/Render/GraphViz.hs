fca14e5a2e03e20d5de79341ab1e734f7b69102e
George Thomas <georgefsthomas@gmail.com>
2020-10-09 11:00:55 +0100

    Handle newlines correctly
    Required specialising, then modifying, the helper function, since the generic version didn't give us the power to treat newlines differently.

--- src/Prettyprinter/Render/GraphViz.hs
+++ src/Prettyprinter/Render/GraphViz.hs
@@ -9,7 +9,6 @@ import qualified Data.Text as T
 
 import Data.GraphViz.Attributes.Complete (Label(HtmlLabel))
 import qualified Data.GraphViz.Attributes.HTML as H
-import Data.Text (Text)
 import Data.Text.Prettyprint.Doc (
     Doc,
     SimpleDocStream(
@@ -33,7 +32,7 @@ render = HtmlLabel . H.Text . render' . layoutPretty defaultLayoutOptions
 
 -- | Render a document stream as HTML text for GraphViz. This provides more fine-grained control than 'render'.
 render' :: SimpleDocStream H.Attributes -> H.Text
-render' = renderSimplyDecorated' (pure .: (renderText . concat)) mempty mempty
+render' = renderSimplyDecorated' mempty mempty
 
 
 {- Util -}
@@ -41,23 +40,19 @@ render' = renderSimplyDecorated' (pure .: (renderText . concat)) mempty mempty
 (.:) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
 (.:) = (.) . (.)
 
--- | This is a very minor modification of 'renderSimplyDecorated', where the /text/ function is
--- additionally passed the current stack. Worth suggesting change upstream.
-renderSimplyDecorated'
-    :: Monoid out
-    => ([ann] -> Text -> out) -- ^ Render plain 'Text'
-    -> (ann -> out)  -- ^ How to render an annotation
-    -> (ann -> out)  -- ^ How to render the removed annotation
-    -> SimpleDocStream ann
-    -> out
-renderSimplyDecorated' text push pop = go []
+
+-- | This is a minor modification of 'renderSimplyDecorated', where the /text/ function is
+-- additionally passed the current stack.
+renderSimplyDecorated' :: ([H.Attribute] -> [H.TextItem]) -> ([H.Attribute] -> [H.TextItem]) -> SimpleDocStream [H.Attribute] -> [H.TextItem]
+renderSimplyDecorated' push pop = go []
   where
+    text = pure .: (renderText . concat)
     go _           SFail               = panicUncaughtFail
     go []          SEmpty              = mempty
     go (_:_)       SEmpty              = panicInputNotFullyConsumed
     go stack       (SChar c rest)      = text stack (T.singleton c) <> go stack rest
     go stack       (SText _l t rest)   = text stack t <> go stack rest
-    go stack       (SLine i rest)      = text stack (T.singleton '\n') <> text stack (textSpaces i) <> go stack rest
+    go stack       (SLine i rest)      = [H.Newline [H.Align H.HLeft]] <> text stack (textSpaces i) <> go stack rest
     go stack       (SAnnPush ann rest) = push ann <> go (ann : stack) rest
     go (ann:stack) (SAnnPop rest)      = pop ann <> go stack rest
     go []          SAnnPop{}           = panicUnpairedPop
